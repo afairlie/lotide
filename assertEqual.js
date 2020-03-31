eb635f7c8865a564ef6640d2a85a6b2792814ba8
afairlie <ariane.fairlie@gmail.com>
2020-03-31 00:20:15 +0000

    custom assert equal function, with emojis!

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,6 @@
-/* 
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log('Assertion Passed ✅ ' + actual + ' === ' + expected + ' 👍')
     : console.log('Assertion Failed 🙁 ' + actual + ' ❗== ' + expected);
-};
- */
-
-// do I get the same results with template literals?
-
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`Assertion Passed ✅ ${actual} === ${expected} 👍`)
-    : console.log(`Assertion Failed 🙁 ${actual} ❗== ${expected}`);
 };
 
 assertEqual('Lighthouse Labs', 'Bootcamp');
