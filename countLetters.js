a00dfe59be82a15e5cfda16f8c5de8fe0532fd80
afairlie <ariane.fairlie@gmail.com>
2020-04-01 22:38:53 +0000

    added test case 2 - name with space, test case 3 - long ass word

--- countLetters.js
+++ countLetters.js
@@ -7,16 +7,27 @@ const countLetters = function(string) {
   let letterCount = {};
 
   for (let letter of string) {
-    if (letterCount[letter] === undefined) {
+    letter = letter.toLowerCase();    // convert to lower case so ex. A and a aren't separate counts
+
+    if (letter === ' ') { // don't count spaces
+      letter++;
+    } else if (letterCount[letter] === undefined) {     // if a value is undefined it doesn't exist in letterCount{}, add key and value count
       letterCount[letter] = 1;
-      console.log(letter);
-    } else if (letterCount[letter]) {
+    } else if (letterCount[letter]) {     // else up the count of an existing letter key by one
       letterCount[letter] += 1;
     }
   }
-  // RETURN SOMETHING
-  console.log(letterCount);
-  return string;
+  return letterCount;
 }
 
-assertEqual(countLetters('Hello'), 'Hello');
+// TEST CASES
+console.log(countLetters('Hello'));
+assertEqual(countLetters('Hello')['l'], 2);
+
+console.log(countLetters('Ariane Fairlie'));
+assertEqual(countLetters('Ariane Fairlie')['i'], 3);
+
+console.log(countLetters('pneumonoultramicroscopicsilicovolcanoconiosis')) // apparently longest word in english dictionary...
+assertEqual(countLetters('pneumonoultramicroscopicsilicovolcanoconiosis')['o'], 9);
+
+// CASES OUT OF SCOPE (for now) - numbers, !?* etc.
