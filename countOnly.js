d0bc054dd220d27b136342b66b6e88fe2a00fc58
afairlie <ariane.fairlie@gmail.com>
2020-04-01 20:36:47 +0000

    use object with name: bool key-value pairs to indicate which values in an array to count

---
+++ countOnly.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`Assertion Passed ✅ ${actual} === ${expected} 👍`)
+    : console.log(`Assertion Failed 🙁 ${actual} ❗== ${expected}`);
+};
+
+// CHALLENGE: receive an array and an object. It will return an object containing counts of everything that the input object listed.
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
