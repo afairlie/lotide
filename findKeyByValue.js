4a64cb2909a84afbe33f7c47d320956984c224b1
afairlie <ariane.fairlie@gmail.com>
2020-04-02 01:51:01 +0000

    added test cases: value = 2, value = undefined

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -20,7 +20,11 @@ const findKeyByValue = function(object, value) {
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama:  "The Wire",
+  not_a_show: 2
 };
 
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), 'sci_fi');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Arrested Development"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, 2), 'not_a_show');
+assertEqual(findKeyByValue(bestTVShowsByGenre, undefined), undefined);
