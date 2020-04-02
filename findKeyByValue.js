c01192dac801ce18c44fc5599c2582b1bb712195
afairlie <ariane.fairlie@gmail.com>
2020-04-02 01:41:34 +0000

    initial commit: find first key by value. One test case

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`Assertion Passed ✅ ${actual} === ${expected} 👍`)
+    : console.log(`Assertion Failed 🙁 ${actual} ❗== ${expected}`);
+};
+
+// CHALLENGE: takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+
+const findKeyByValue = function(object, value) {
+  let valuesArr = Object.values(object);
+  let keysArr = Object.keys(object);
+
+  for (let each in keysArr) {
+    if (value === valuesArr[each]) {
+      return keysArr[each];
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
