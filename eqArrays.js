8c35ea4abdb3da5a40d47e275041feb7e99271b7
afairlie <ariane.fairlie@gmail.com>
2020-04-02 01:16:03 +0000

    refactored: eqArrays return false if input not arrays, default return is false

--- eqArrays.js
+++ eqArrays.js
@@ -4,18 +4,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  if (Array.isArray(arr1) && Array.isArray(arr2)) {
+    if (arr1.length !== arr2.length) {
       return false;
     }
+  
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
   }
-  return true;
+  return false;
 }
 
+// TEST CASES
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
@@ -23,3 +27,8 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+
+// CASES WHERE INPUT NOT ARRAY
+assertEqual(eqArrays('hello', [1, 2, 3, 4]), false);
+assertEqual(eqArrays(null, [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2], null), false);
