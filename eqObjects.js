aeaa4ee8cab0a0a39329ac54156478b056f0593d
afairlie <ariane.fairlie@gmail.com>
2020-04-02 12:32:07 +0000

    major overhaul: modified first test case, starting function from scratch.

--- eqObjects.js
+++ eqObjects.js
@@ -22,21 +22,11 @@ const eqArrays = function(arr1, arr2) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  let object1Vals = Object.values(object1);
-  let object2Vals = Object.values(object2);
 
-  let object1Keys = Object.keys(object1);
-  let object2Keys = Object.keys(object2);
-
-  if (eqArrays(object1Keys, object2Keys) && eqArrays(object1Vals, object2Vals)) {
-    return true;
-  } else {
-    return false;
-  }
 };
 
 const testObj1 = {a: 1, b: 2, c: 3};
-const testObj2 = {a: 1, b: 2, c: 3};
+const testObj2 = {b: 2, a: 1, c: 3};
 assertEqual(eqObjects(testObj1, testObj2), true);
 
 const testObj3 = {a: 1, x: 2, c: 3}; // fail
@@ -46,5 +36,13 @@ assertEqual(eqObjects(testObj3, testObj4), false);
 const testObj5 = {a: '1', b: 2, c: 3}; // fail
 const testObj6 = {a: 1, b: 2, c: 3};
 assertEqual(eqObjects(testObj5, testObj6), false);
+
+const testObj7 = {a: [1, 2, 3], b: 2, c: 3};
+const testObj8 = {a: [1, "2", 3], b: 2, c: 3};
+assertEqual(eqObjects(testObj7, testObj8), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
 
 // OTHER EDGE CASES: object not put in, empty object? 
