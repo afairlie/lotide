683f2a337697c5e260068f25450f2e9db87bdfcd
afairlie <ariane.fairlie@gmail.com>
2020-04-06 17:23:17 +0000

    removed comments unecessary comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,3 @@
-/* const assertEqual = function(actual, expected) {
-  actual === expected ? console.log('Assertion Passed ✅ ' + actual + ' === ' + expected + ' 👍')
-    : console.log('Assertion Failed 🙁 ' + actual + ' ❗== ' + expected);
-}; */
-
-// do I get the same results with template literals?
-
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`Assertion Passed ✅ ${actual} === ${expected} 👍`)
     : console.log(`Assertion Failed 🙁 ${actual} ❗== ${expected}`);
